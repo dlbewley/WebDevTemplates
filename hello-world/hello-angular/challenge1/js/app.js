cfcdb2c982c9722871649eb398495ce7215b852b
Dale Bewley <dlbewley@pixar.com>
2015-03-12 17:43:19 -0700

    challenge1

---
+++ hello-world/hello-angular/challenge1/js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var hello = angular.module('hello', ['hello.controllers', 'hello.directives']);
+
+///////////////////////////////////////////////////////////////////////////////
+angular.module('hello.controllers', []).
+///////////////////////////////////////////////////////////////////////////////
+
+  // Main Controller
+  controller('MainCtrl', ['$scope', function ($scope) {
+    var hello_msg = 'Some Awesome Things';
+    $scope.hello_msg = hello_msg;
+    var date_time = new Date();
+    console.log(date_time);
+    $scope.date_time = date_time;
+  }]).
+
+  // Awesome Controller
+  controller('AwesomeCtrl', ['$scope', function ($scope) {
+    var awesome_things = [
+      {"name":   "California"},
+      {"name":   "Backpacking"},
+      {"name":   "Chess"},
+      {"name":   "Beer"},
+      {"name":   "Bikes"},
+      {"name":   "NASA"}
+    ];
+    angular.forEach(awesome_things, function(thing) {
+      thing.name += '!';
+    });
+    $scope.awesome_things = awesome_things;
+  }]);
+
+///////////////////////////////////////////////////////////////////////////////
+angular.module('hello.directives', []).directive('awesome', [function () {
+///////////////////////////////////////////////////////////////////////////////
+    return {
+      restrict: 'E',
+      controller: 'AwesomeCtrl',
+      templateUrl: 'partials/awesome.html'
+    };
+  }]);
